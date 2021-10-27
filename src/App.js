3ee2370a76c9e64bab29f198e8110da6a033804f
Eric Barber <eric.a.barber@gamil.com>
2021-10-27 16:03:00 -0700

    Lesson 72 complete

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-import HomePage from './homepage.component';
+import HomePage from './pages/homepage/homepage.component';
 
 function App() {
-  return <div>
-    <HomePage />
-  </div>;
+  return (
+    <div>
+      <HomePage />
+    </div>
+  );
 }
 
 export default App;
